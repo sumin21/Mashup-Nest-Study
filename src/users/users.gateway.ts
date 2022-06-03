4fc06a86d228b152caad6cb046b281a06648b319
sumin21 <tnals1178@naver.com>
2022-06-04 00:43:09 +0900

    websocket

--- src/users/users.gateway.ts
+++ src/users/users.gateway.ts
@@ -11,6 +11,7 @@ import { Server, Socket } from 'socket.io';
 import { Logger } from '@nestjs/common';
 
 @WebSocketGateway(5000, {
+  transports: ['websocket'],
   namespace: 'users',
 })
 export class UsersGateway {
@@ -25,27 +26,23 @@ export class UsersGateway {
 
   private logger: Logger = new Logger('EventsGateway');
 
-  public handleConnection(client): void {
+  public handleConnection(client: Socket): void {
     console.log('hi');
-    client['id'] = String(Number(new Date()));
-    client['nickname'] = '유저' + String(Number(new Date()));
-    this.client[client['id']] = client;
+    this.client[client.id] = client;
     this.logger.log(`Client Connected : ${client.id}`);
   }
 
-  public handleDisconnect(client): void {
-    console.log('bye', client['id']);
-    delete this.client[client['id']];
+  public handleDisconnect(client: Socket): void {
+    console.log('bye', client.id);
+    delete this.client[client.id];
   }
 
   @SubscribeMessage('message')
   handleMessage(
     @MessageBody() data: string, // 클라이언트로부터 들어온 데이터
-
     @ConnectedSocket() client: Socket,
   ) {
-    const res: string = data;
-    console.log(client.id);
+    const res: string = data + ' ' + client.id;
     this.logger.log(data);
     this.server.emit('msgToClient', res);
   }
